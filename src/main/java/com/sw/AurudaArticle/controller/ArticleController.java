9378d877a82bd7451dfa9b9145faf657c5f3e6a1
crane0731 <dlwnsgkr8318@naver.com>
2024-11-26 11:22:06 +0900

    .

--- src/main/java/com/sw/AurudaArticle/controller/ArticleController.java
+++ src/main/java/com/sw/AurudaArticle/controller/ArticleController.java
@@ -97,7 +97,7 @@ public class ArticleController {
         articleService.upRecommendation(articleId);
         Article article = articleService.findArticleById(articleId);
         userService.upPoint(article.getUser().getId());
-        System.out.println("Asdassd");
+        System.out.println("Asdasssd");
 
         return ResponseEntity.ok("게시물 추천 성공");
     }
